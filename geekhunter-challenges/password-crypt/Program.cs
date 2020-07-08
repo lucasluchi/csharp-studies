55569f547589190149aa5076b0772e44ede8a68e
lucasluchi <lds.lucasluchi@gmail.com>
2020-07-08 19:44:21 -0300

    Challenge password-crypt

---
+++ geekhunter-challenges/password-crypt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace password_crypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
